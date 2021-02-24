1d1b2b073ffe30ad77aa8fd78ec5897e170e5e7b
Egor Korshunov <11524779+EgorKorshunov@users.noreply.github.com>
2021-02-24 11:44:46 +0100

    Update multiples-of-3-or-5.js

--- multiples-of-3-or-5.js
+++ multiples-of-3-or-5.js
@@ -18,3 +18,15 @@ function solution(number){
     return acc + num;
   }, 0);
 }
+
+// The previous solution is strange, this is better
+function solution2(number){
+  if (number <= 0) return 0;
+  let sum = 0;
+  for (let i = 1; i < number; i++) {
+    if (!(i % 3) || !(i % 5)) {
+      sum +=i;
+    } 
+  }
+  return sum;
+}
